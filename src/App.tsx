8b362e5086150f96ee4a055a16c72171cc849fef
isoftchamp <qwertroot122@outlook.com>
2024-10-01 17:09:00 +0000

    feat: :sparkles: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <div className="bg-white p-8 rounded shadow">Subly card</div>
+    </div>
+  );
+};
+
+export default App;
